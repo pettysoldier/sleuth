ab8b9cde45089cd65ffed25541af228dadcc6d65
zhushengguo <3xtr3m3S0ftwar3>
2020-07-24 13:40:05 +0800

    Initial commit

---
+++ service-b/src/main/java/richborn/controller/IndexController.java
@@ -0,0 +1,31 @@
+package richborn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhusg
+ * @date 2020/7/21 15:45
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/Second")
+    public String index(){
+        //远程调用service-b的方法，service-b分别调用service-c、service-b
+        String result = remoteCallServiceB();
+        return " service-b start ," + result;
+    }
+
+    //远程调用service-b
+    private String remoteCallServiceB(){
+        String url = "http://service-c/Third";
+        String result = restTemplate.getForObject(url , String.class);
+        return result;
+    }
+}
